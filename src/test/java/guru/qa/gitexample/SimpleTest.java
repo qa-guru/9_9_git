5e847ffa8eec8fc31a06332d54c24a0345c8081d
dtuchs <d.tuchs@propellerads.net>
2021-11-18 21:01:29 +0300

    added first simple test

---
+++ src/test/java/guru/qa/gitexample/SimpleTest.java
@@ -0,0 +1,12 @@
+package guru.qa.gitexample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
